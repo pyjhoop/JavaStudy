504b3d599ebcbf9fa048cba951c84d1f62f1fa0e
pyjhoop <pyjhoop@naver.com>
2022-10-25 19:09:18 +0900

    review
    review

---
+++ YeonjunPark/03_ControlReview/src/run/Run.java
@@ -0,0 +1,21 @@
+package run;
+
+import control.A_If;
+import control.B_Switch;
+
+public class Run {
+	public static void main(String[] args) {
+		A_If a = new A_If();
+//		a.method1();
+//		a.method2();
+//		a.method3();
+//		a.method4();
+//		a.method5();
+//		a.method6();
+		B_Switch b= new B_Switch();
+//		b.method1();
+//		b.method2();
+//		b.method3();
+		b.method4();
+	}
+}
