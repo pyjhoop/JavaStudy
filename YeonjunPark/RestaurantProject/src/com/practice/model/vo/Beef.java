2a458e46c80e837ae5d3e6e36b01127cc2338458
pyjhoop <pyjhoop@naver.com>
2022-11-16 14:45:36 +0900

    hg
    df

---
+++ YeonjunPark/RestaurantProject/src/com/practice/model/vo/Beef.java
@@ -0,0 +1,19 @@
+package com.practice.model.vo;
+
+public class Beef extends Food{
+
+	public Beef() {}
+
+	public Beef(int index, String name, int price) {
+		super(index, name, price);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	
+	
+	
+}
