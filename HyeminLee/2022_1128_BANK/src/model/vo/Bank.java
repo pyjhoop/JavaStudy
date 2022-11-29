73a194d5dc5ef05d2fb015aaba4b6da0935171c6
biho <mminimi27@gmail.com>
2022-11-29 15:09:19 +0900

    Update Bank.java

--- HyeminLee/2022_1128_BANK/src/model/vo/Bank.java
+++ HyeminLee/2022_1128_BANK/src/model/vo/Bank.java
@@ -1,25 +1,71 @@
 package model.vo;
 
 public class Bank {
-
 	private String accountNumber;
 	private String name;
+	private String id;
 	private String pwd;
-	private String balance; //잔고
-//	private ArrayList<String> breakdown;// 내역 크기 5로 5이상되면 오래된 것 부터 삭제 
+	private int balance;
+	public static final int SIZE = 5;
+	
+	public Bank() {}
+
+	public Bank(String accountNumber, String name, String id,String pwd, int balance) {
+		this.accountNumber = accountNumber;
+		this.id = id;
+		this.name = name;
+		this.pwd = pwd;
+		this.balance = balance;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return name+"님의 " + accountNumber + ", name=" + name + ", id=" + id + ", pwd=" + pwd
+				+ ", balance=" + balance + "]";
+	}
+
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-} // bank
+}
